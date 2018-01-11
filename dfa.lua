983dc5c8300e2fa72e8bf07d0d5b70cb672104a4
Aaron Schmidlkofer <ajs130430@utdallas.edu>
2018-01-10 21:36:47 -0600

    Clarified and cleaned up some code in dfa.lua

--- dfa.lua
+++ dfa.lua
@@ -1,30 +1,28 @@
-function importDFA(filename)
+function importDFA(filename) -- Wrapper Function for buildDFA, allows filebased import
 	dfaTable = require(filename)
 	return buildDFA(dfaTable.states, dfaTable.finalStates, dfaTable.startState, dfaTable.sigma, dfaTable.delta)
 end
 
 function buildDFA (states, finalStates, startState, sigma, delta)
-	--read transition function
-	local funTable = {}
+	local funTable = {} --Build a table of functions, one for each state
 	for _,v in ipairs(states) do
-
 		funTable[v] = function(str, index, seq)
 			seq = seq .. ", " .. v
-			if(str:len() <  index) then return finalStates[v],seq end --if at end of string, check if we are a final state
+			if(str:len() <  index) then return finalStates[v],seq or false end --if at end of string, check if we are a final state
 			
 			local cur = str:sub(index,index) --read the character
-			--if the char is in sigma, then call the next state, otherwise fail
+
+			--if the char is in the state's transition table, then call the next state, otherwise fail
 			targetFunction = funTable[delta[v][cur]]
 			if targetFunction then return targetFunction(str, index+1, seq)
 			else return false,seq end
 		end			
 	end
 
-	return function(str) --this is just a wrapper function to convert the output to true/false and build the first call 
-		
-		accept,seq = funTable[startState](str,1,'')
+	return function(str) --Wrapper function to convert the output to true/false and build the first call 
+		accept, seq = funTable[startState](str,1,'')
 		seq = seq:sub(3,seq:len())
 		
-		return accept or false,seq
+		return accept, seq
 	end
 end
